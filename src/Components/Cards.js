8f4605405894086722eb94b6b2e4351424874fe9
vantheman2210 <martinovic.ba@gmail.com>
2022-07-12 13:23:29 +0200

    added src to card img for dynamic loading

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -4,10 +4,17 @@ const Cards = (props) => {
 	return (
 		<div className="cards">
 			{[ ...Array(12) ].map((x, i) => {
-				return <div key={i} className={`card${i}`}>Hello</div>
+				return (
+					<div key={i} className={`card${i}`}>
+						<img alt="img" src={require('../Images/joker.jpg')} />
+						<p>Villain</p>
+					</div>
+				);
 			})}
 		</div>
 	);
 };
+
+/*require('../Images/joker.jpg').default*/
 
 export default Cards;
